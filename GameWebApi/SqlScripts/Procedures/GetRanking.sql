576138ee5bbf36bf276fbf46b8a294a9b7108a85
PatrykSzereluk <27899505+PatrykSzereluk@users.noreply.github.com>
2020-08-19 23:30:43 +0200

    Added procedures

---
+++ GameWebApi/SqlScripts/Procedures/GetRanking.sql
@@ -0,0 +1,49 @@
+﻿IF(EXISTS(SELECT * FROM SYS.OBJECTS WHERE OBJECT_ID = OBJECT_ID('[Web].[GetRanking]') AND [Type] = 'P'))
+DROP PROCEDURE [Web].[GetRanking]
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE or ALTER PROCEDURE [Web].[GetRanking]
+	@Take int,
+	@Skip int,
+	@RankingCategory int, -- 1 - Kills, 2 - Assists ( LATER Exp/lvl), 3 - GamesPlayed
+	@Order bit
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @Sql NVARCHAR(MAX)
+
+	SET @Sql = 'SELECT Nick,
+		   [PS].Kills, 
+		   [PS].Deaths, 
+		   [PS].Assists, 
+		   [PS].GamesPlayed, 
+		   [PS].GamesWon, 
+		   [PS].GameLose 
+	FROM Common.PlayerIdentity [PI]
+	LEFT JOIN Common.PlayerStatistics [PS] ON [PI].ID = [PS].PlayerId
+	ORDER BY [_CATEGORY_] [_ORDER_]'
+	
+	IF(@RankingCategory = 2)
+		SET @Sql = REPLACE(@Sql,'[_CATEGORY_]', '[PS].Assists')
+	ELSE IF(@RankingCategory = 3)
+		SET @Sql = REPLACE(@Sql,'[_CATEGORY_]', '[PS].GamesPlayed')
+	ELSE
+		SET @Sql = REPLACE(@Sql,'[_CATEGORY_]', '[PS].Kills')
+	IF(@Order = 0)
+		SET @Sql = REPLACE(@Sql,'[_ORDER_]', 'DESC')
+	 ELSE
+		SET @Sql = REPLACE(@Sql,'[_ORDER_]', 'ASC')
+
+
+	exec sp_executesql @Sql
+
+END
+GO
+
+--exec [Web].[GetRanking] 0,0,0,0
+
---
+++ GameWebApi/SqlScripts/RandData.sql
@@ -0,0 +1,29 @@
+﻿delete from Common.PlayerDates
+delete from Common.PlayerSalt
+delete from Common.PlayerStatistics
+delete from Common.PlayerIdentity
+
+
+DECLARE @i int = 0
+
+WHILE @i < 46
+BEGIN
+    SET @i = @i + 1
+	declare @nick nvarchar(32) = (select 'nick' + cast(@i as nvarchar))
+  exec common.RegisterNewPlayer 'elo2','pass1',@nick,'em1','sa'
+END
+
+DECLARE @i int = 25
+
+WHILE @i <= 71
+BEGIN
+    SET @i = @i + 1
+	update Common.PlayerStatistics set
+	Kills = FLOOR(RAND()*(250-10+1))+10,
+	Deaths = FLOOR(RAND()*(250-10+1))+10,
+	Assists = FLOOR(RAND()*(250-10+1))+10,
+	GamesWon = FLOOR(RAND()*(250-10+1))+10,
+	GamesPlayed = FLOOR(RAND()*(250-10+1))+10,
+	GameLose = FLOOR(RAND()*(250-10+1))+10
+	where PlayerId = @i
+END
