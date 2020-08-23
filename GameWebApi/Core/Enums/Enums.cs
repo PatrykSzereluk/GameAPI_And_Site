71c2170340f73b19eaf8c2d0782594f35df86aa5
PatrykSzereluk <27899505+PatrykSzereluk@users.noreply.github.com>
2020-08-23 20:35:57 +0200

    Added Core project

---
+++ GameWebApi/Core/Enums/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Enums
+{
+
+    public enum ClanFunction
+    {
+        Leader = 1,
+        Admin = 2,
+        Member = 3
+    }
+}
