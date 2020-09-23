d9bccaa9ec1b2b96d91f40032e25ea544b5973d5
PatrykSzereluk <27899505+PatrykSzereluk@users.noreply.github.com>
2020-09-24 00:02:10 +0200

    Upgrade ranking component

--- ClientSite/GameSite/src/app/Components/ranking/ranking.component.ts
+++ ClientSite/GameSite/src/app/Components/ranking/ranking.component.ts
@@ -9,19 +9,29 @@ import { UserRanking } from '../../Models/Identity/UserRanking';
 })
 export class RankingComponent implements OnInit {
 
+  currentPage = 0;
+
   constructor(private rankingService: RankingService) { }
 
   usersRanking: Array<UserRanking>;
 
   ngOnInit(): void {
-    this.getRanking();
+    this.getRankingData(2,0);
   }
 
-  getRanking() {
-    this.rankingService.getRanking({Take : 10, Skip : 0, RankingCategory : 1, Order : false}).subscribe( t => {
+  getRankingData(take: number, skip: number) {
+    this.rankingService.getRanking({Take : take, Skip : skip, RankingCategory : 1, Order : false}).subscribe( t => {
       this.usersRanking = t;
     });
   }
 
+  getRanking(option: string) {
+    if (option === 'more') {
+      this.currentPage++;
+    } else {
+      this.currentPage--;
+    }
+    this.getRankingData(2, 2 * this.currentPage);
+  }
 
 }
